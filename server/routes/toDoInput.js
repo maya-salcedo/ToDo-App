27219b94e0d3cea6fb5d714ec737a007e406c004
maya <maya.salcedo07@gmail.com>
2021-04-19 14:29:06 +0300

    backend not yet connecting to db

--- server/routes/toDoInput.js
+++ server/routes/toDoInput.js
@@ -1,14 +1,30 @@
 const express = require('express');
-// const pool = require("../db");
+const pool = require('../db');
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    try {
-        const {Item} = req.body;
-        console.log(Item);
-    } catch (err) {
-        console.erro(err.message);
-    }
+  try {
+    const { Item } = req.body;
+    console.log(Item);
+    const newItem = await pool.query(
+      `INSERT INTO todolist(item) VALUES($1) RETURNING *`, [Item]
+    );
+    res.json(newItem.rows[0]);
+    const MyItem = newItem.rows[0];
+    console.log(MyItem);
+} catch (err) {
+    console.error(err.message);
+    res.status(500).send('Something Went Wrong');
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const allItems = await pool.query("SELECT * FROM todolist");
+    res.json(allItems.rows);
+  } catch (err) {
+    console.err(err.message);
+  }
 });
 
 module.exports = router;
