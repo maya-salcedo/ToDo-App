683c319cc5e5408579a2b2192e6d1586480e8c2c
maya <maya.salcedo07@gmail.com>
2021-08-30 09:51:57 +0300

    added proxy for API request

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require("cors");
+const cors = require('cors');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const todoinput = require('./routes/toDoInput');
@@ -26,12 +26,12 @@ app.use('/users', usersRouter);
 app.use('/todoinput', todoinput);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
