ea3cc84e4a9e50b9af831f13aabafacb7efe86f3
maya <maya.salcedo07@gmail.com>
2022-01-12 13:42:35 +0200

    managed the type annotation in styled-components

--- client/src/elements/ContainerWrapper.tsx
+++ client/src/elements/ContainerWrapper.tsx
@@ -37,7 +37,7 @@ const StyledContainer = styled.div`
   }
 `;
 
-const ContainerWrapper = ({ children }) => (
+const ContainerWrapper: React.FC = ({ children }) => (
   <StyledContainer>{children}</StyledContainer>
 );
 
